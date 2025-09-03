4e8b5b219d148cedda658bf8d24557953e6009b0
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-09-03 13:36:31 -0300

    avance de algunos #todo

--- guia/semana1/main.cpp
+++ guia/semana1/main.cpp
@@ -9,21 +9,30 @@ using namespace std;
 // -----------------------------------------------------------------------------
 void swap_por_puntero(int* a, int* b) {
     // TODO: implementar intercambio clásico usando *a, *b y una variable temporal.
-    // int tmp = *a;
-    // *a = *b;
-    // *b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 void swap_si_mayor(int* p, int* q) {
     // TODO: intercambiar solo si *p > *q.
+    if (*p > *q) {
+        swap_por_puntero(p, q);
+    }   
 }
 
 void swap_si_menor(int* p, int* q) {
     // TODO (opcional): intercambiar solo si *p < *q.
+    if (*p < *q) {
+        swap_por_puntero(p, q);
+    }   
 }
 
 void set_si_positivo(int* p, int valor) {
     // TODO (opcional): escribir 'valor' en *p solo si valor > 0.
+    if (valor > 0) {
+        *p = valor;
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -31,16 +40,25 @@ void set_si_positivo(int* p, int valor) {
 // -----------------------------------------------------------------------------
 int suma(const int* arr, int n) {
     // TODO: recorrer arr[0..n-1] y acumular.
-    return 0;
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total += arr[i];
+    }
+    return total;   
 }
 
 double promedio(const int* arr, int n) {
     // TODO: usar suma(...) y devolver (double)suma/n si n>0; si n==0 retornar 0.0.
+    if (n == 0) return 0.0;
+    else{
+        return (double)suma(arr, n) / n;
+    }
     return 0.0;
 }
 
 int maximo(const int* arr, int n) {
     // TODO (opcional): devolver el mayor valor del arreglo (validar n>0).
+    
     return 0;
 }
 
