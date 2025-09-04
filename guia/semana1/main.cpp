e2edee0e1377cb9d33f8ae3c59da91de52ad6636
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-09-03 23:26:32 -0300

    semana 1 done

--- guia/semana1/main.cpp
+++ guia/semana1/main.cpp
@@ -58,53 +58,117 @@ double promedio(const int* arr, int n) {
 
 int maximo(const int* arr, int n) {
     // TODO (opcional): devolver el mayor valor del arreglo (validar n>0).
-    
-    return 0;
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int main() {
-    cout << fixed << setprecision(2);
-
+    
+    cout << fixed << setprecision(2) << " " << endl;
     // -------------------------------------------------------------------------
     // (A) Variables y direcciones (calentamiento)
     // -------------------------------------------------------------------------
     // TODO:
     // 1) Declarar: int a=5; float f=3.14f; char c='Z';
+    int a = 5;
+    float f = 3.14f;
+    char c = 'Z';
     // 2) Imprimir valor y dirección:
-    //    cout << "a=" << a << "  &a=" << &a << "\n";
-    //    cout << "f=" << f << "  &f=" << &f << "\n";
+    
+    cout << "a=" << a << "  &a=" << &a << "\n";
+    cout << "f=" << f << "  &f=" << &f << "\n";
+    
     //    Para 'c' usar:
-    //    cout << "c=" << c << "  &c=" << static_cast<const void*>(&c) << "\n";
+    
+    cout << "c=" << c << "  &c=" << static_cast<const void*>(&c) << "\n";
+    
     // 3) Escribir en comentarios 2-3 observaciones sobre el patrón de direcciones.
-
+    // Las direcciones de memoria son diferentes para cada variable.
+    // La dirección de 'a' es menor que la de 'f', y la de 'f' es menor que la de 'c'.
+    // Esto puede deberse al orden de declaración y al tamaño de cada tipo de dato.
+    
+    cout << " " << endl;
+    
     // -------------------------------------------------------------------------
     // (B) Proyecto CasaDomotica++ - Parte 1: Sensor de temperatura por puntero
     // -------------------------------------------------------------------------
     // El "sensor" será una variable 'temperaturaActual' y un puntero a ella.
     // TODO:
     // 1) Declarar: int temperaturaActual = 22;
+    
+    int temperaturaActual = 22;
+    
     // 2) Declarar: int* ptrTemp = &temperaturaActual;
+    
+    int* ptrTemp = &temperaturaActual;
+    
     // 3) Mostrar equivalencia: *ptrTemp y temperaturaActual.
+    
+    cout << "temperaturaActual = " << temperaturaActual << "\n";
+    cout << "*ptrTemp = " << *ptrTemp << "\n";
+
     // 4) Modificar el valor a través del puntero: *ptrTemp = 27; y verificar.
+    *ptrTemp = 27;
+    cout << "temperaturaActual = " << temperaturaActual << "\n";
+    cout << "*ptrTemp = " << *ptrTemp << "\n";    
+
     // 5) Leer una nueva temperatura desde consola y asignarla vía *ptrTemp.
+    cout << "Ingrese nueva temperatura: ";
+    int nuevaTemp;
+    cin >> nuevaTemp;
+    *ptrTemp = nuevaTemp;
+    cout << "temperaturaActual = " << temperaturaActual << "\n";
+    cout << "*ptrTemp = " << *ptrTemp << "\n";
 
     // -------------------------------------------------------------------------
     // (C) Paso por puntero: pruebas de swap y variantes
     // -------------------------------------------------------------------------
+    cout << "\n" << endl;
     // TODO:
     // 1) Implementar swap_por_puntero arriba y probar con:
     //    int x=5, y=9;  swap_por_puntero(&x,&y);
+    
+    int x = 5, y = 9;
+    cout << "Antes de swap: x=" << x << ", y=" << y << "\n";
+    swap_por_puntero(&x, &y);
+    cout << "Después de swap: x=" << x << ", y=" << y << "\n";
+    
     // 2) Implementar swap_si_mayor y probar casos (p.ej., 10 y 3, luego 2 y 7).
+    int m = 10, n = 3;
+    cout << "Antes de swap_si_mayor: m=" << m << ", n=" << n << "\n";
+    swap_si_mayor(&m, &n);
+    
     // 3) (Opcional) swap_si_menor y set_si_positivo.
+
+    cout << " " << endl;
 
     // -------------------------------------------------------------------------
     // (D) Arreglos estáticos: direcciones y funciones con punteros
     // -------------------------------------------------------------------------
     // 1) Declarar un arreglo fijo: int v[5] = {10,20,30,40,50};
+    
+    int v[5] = {10, 20, 30, 40, 50};
+    
     // 2) Imprimir v[i] y &v[i] en un for (i=0..4) para observar contigüidad.
     //    Sugerencia de línea para dirección:
     //    cout << (void*)(&v[i]) << "\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "v[" << i << "]=" << v[i] << "  &v[" << i << "]=" << (void*)(&v[i]) << "\n";
+    }
+    
+    
     // 3) Llamar a suma(v,5), promedio(v,5) y (opcional) maximo(v,5) e imprimir.
+    
+    cout << "Suma: " << suma(v, 5) << "\n";
+    cout << "Promedio: " << promedio(v, 5) << "\n";
+    cout << "Máximo: " << maximo(v, 5) << "\n";
+       
+    
     //    // TODO: completar implementaciones arriba.
     cout << "\nFin Semana 1. Próximo paso: memoria dinámica y archivos (Semana 2).\n";
     return 0;
