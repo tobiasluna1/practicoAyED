b875a27e7497836bb3c12f81070acd6ca53b8e0d
tobiasluna1 <tobiasluna1@mi.unc.edu.ar>
2025-09-04 11:30:45 -0300

    semana 2 terminado

--- guia/semana2/src/readings.cpp
+++ guia/semana2/src/readings.cpp
@@ -13,16 +13,51 @@ float* crear_arreglo(int size) {
 
 void liberar_arreglo(float*& arr) {
     //TODO
+    delete[] arr;
+    arr = nullptr;
 }
 
 int cargar_desde_archivo(const std::string& ruta, float* arr, int size) {
     //TODO
+    if (arr == nullptr || size <= 0)    return 0;
+    else {
+        std::ifstream file(ruta);
+        if (!file.is_open()) {
+            return 0;
+        }
+        int count = 0;
+        while (count < size && file >> arr[count]) {
+            count++;
+        }
+        file.close();
+        return count;
+    }   
 }
 
 bool guardar_en_archivo(const std::string& ruta, const float* arr, int size) {
     // TODO
+    if (arr == nullptr || size <= 0) {
+        return false;
+    } else {
+        std::ofstream file(ruta);
+        if (!file.is_open()) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            file << std::fixed << std::setprecision(2) << arr[i] << "\n";
+        }
+        file.close();
+        return true;
+    }
 }
 
 void imprimir(const float* arr, int size) {
     // TODO
+    if (arr == nullptr || size <= 0) {
+        std::cout << "No hay datos para mostrar." << std::endl;
+    } else {
+        for (int i = 0; i < size; i++) {
+            std::cout << std::fixed << std::setprecision(2) << arr[i] << "\n";
+        }
+    }
 }
